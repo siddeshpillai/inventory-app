87b3514f1150c1b559f72bedb5bc8e3833b9a5f5
Siddesh Pillai <siddeshvijji@gmail.com>
2021-06-29 19:32:25 -0700

    Code Refactor

--- inventory-app/app/src/main/java/com/example/inventory/InventoryApplication.kt
+++ inventory-app/app/src/main/java/com/example/inventory/InventoryApplication.kt
@@ -18,7 +18,9 @@ package com.example.inventory
 import android.app.Application
 import com.example.inventory.data.ItemRoomDatabase
 
-class InventoryApplication : Application() {
 
-    val database: ItemRoomDatabase by lazy { ItemRoomDatabase.getDatabase(this)}
+class InventoryApplication : Application() {
+    // Using by lazy so the database is only created when needed
+    // rather than when the application starts
+    val database: ItemRoomDatabase by lazy { ItemRoomDatabase.getDatabase(this) }
 }
